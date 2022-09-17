84fe1db1e7bf56e7dee8932cba39df049b398b3a
Mark Silva <mark.allen.silva@gmail.com>
2022-09-16 21:31:44 -0700

    test

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const h1 = document.createElement("h1")
-h1.textContent = "Yo yo, world"
-h1.className = "header"
-console.log(h1)
-// document.getElementById("root").append(h1)
+// const page = (
+//   <div>
+//     <h1>Stuff and stuffs</h1>
+//     <p>garbage</p>
+//   </div>
+// )
 
+const navBar = (
+  <nav>
+    <h1>Dookie</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Cows</li>
+    </ul>
+  </nav>
+)
 
-const element = <h1 className="header">Hello</h1>
-console.log(element)
-
-ReactDOM.render(element, document.getElementById("root"))
+ReactDOM.render(
+  navBar,
+  document.getElementById("root")
+)
