1f2dc2c2672a6fc7b9aab3c311d548049008a0e4
Mark Silva <mark.allen.silva@gmail.com>
2022-09-16 21:10:53 -0700

    testexit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-ReactDOM.render(
-  <ul>
-    <li>fart</li>
-    <li>poot</li>
-  </ul>, 
-  document.getElementById("root"))
+const h1 = document.createElement("h1")
+h1.textContent = "Yo yo, world"
+h1.className = "header"
+console.log(h1)
+// document.getElementById("root").append(h1)
+
+
+const element = <h1 className="header">Hello</h1>
+console.log(element)
+
+ReactDOM.render(element, document.getElementById("root"))
