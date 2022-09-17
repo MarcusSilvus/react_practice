42c0eaf791df2c9c9c06759a4a4a2d7b232d968a
Mark Silva <mark.allen.silva@gmail.com>
2022-09-17 16:19:29 -0700

    learning components

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+ function MyPage() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src="react-icon.svg" width="40px"/>
+        </nav>
+      </header>
+      <h1>Why I want to learn stuffs</h1>
+        <ol>
+          <li>Learn things</li>
+          <li>Make things</li>
+          <li>Clap things</li>
+          <li>Yelp things</li>
+        </ol>
+      <footer>
+        <h6>&copy; 2022 Dookie Master. All rights reserved</h6>
+      </footer>
+    </div>
+  )
+ }
 
-
-const page = (
-  <div>
-    <img src="react-icon.svg" height="50" width="50"></img>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was released in 2013</li>
-      <li>Was orginally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-)
-
-ReactDOM.render(
-  page,
+ ReactDOM.render(
+  <MyPage />, 
   document.getElementById("root")
-)
+ )
