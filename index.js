6d5e6028fc1823c5c636f7c29c3bfc474b8c3aab
Mark Silva <mark.allen.silva@gmail.com>
2022-09-17 14:36:57 -0700

    simple page

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-// const page = (
-//   <div>
-//     <h1>Stuff and stuffs</h1>
-//     <p>garbage</p>
-//   </div>
-// )
 
-const navBar = (
-  <nav>
-    <h1>Dookie</h1>
+
+const page = (
+  <div>
+    <img src="react-icon.svg" height="50" width="50"></img>
+    <h1>Fun facts about React</h1>
     <ul>
-      <li>Pricing</li>
-      <li>About</li>
-      <li>Cows</li>
+      <li>Was released in 2013</li>
+      <li>Was orginally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
     </ul>
-  </nav>
+  </div>
 )
 
 ReactDOM.render(
-  navBar,
+  page,
   document.getElementById("root")
 )
